73002af9c9db0286f138ff403d5a26d27924c5b4
Tsukasa Gotoh <go10.2ka3@ezweb.ne.jp>
2018-06-01 15:45:46 +0900

    数字を送信できるようになった

--- app/src/main/java/jp/ac/titech/itpro/sdl/serviceex1/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/serviceex1/MainActivity.java
@@ -1,6 +1,9 @@
 package jp.ac.titech.itpro.sdl.serviceex1;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,7 +17,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate in " + Thread.currentThread());
         setContentView(R.layout.activity_main);
-
+        BroadcastReceiver broadCastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                Log.d(TAG, "onReceive: " + action);
+                if (action == null) return;
+                switch (action) {
+                    case TestService3.ACTION_ANSWER:
+                        Log.d(TAG, "ACTION_ANSWERを受信");
+                        Bundle ex = intent.getExtras();
+                        Log.d(TAG, "EXTRA_ANSWER : " + ex.getInt(TestService3.EXTRA_ANSWER));
+                        break;
+                }
+            }
+        };
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(TestService3.ACTION_ANSWER);
+        registerReceiver(broadCastReceiver, filter);
     }
 
     public void pressTest1(View v) {
@@ -25,7 +45,11 @@ public class MainActivity extends AppCompatActivity {
         testService2();
     }
 
-    private void testService1() {
+    public void pressTest3(View v) {
+        testService3();
+    }
+
+        private void testService1() {
         Log.d(TAG, "testService1 in " + Thread.currentThread());
         Intent intent = new Intent(this, TestService1.class);
         intent.putExtra(TestService1.EXTRA_MYARG, "Hello, Service1");
@@ -36,6 +60,13 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "testService2 in " + Thread.currentThread());
         Intent intent = new Intent(this, TestService2.class);
         intent.putExtra(TestService2.EXTRA_MYARG, "Hello, Service2");
+        startService(intent);
+    }
+
+    private void testService3() {
+        Log.d(TAG, "testService3 in " + Thread.currentThread());
+        Intent intent = new Intent(this, TestService3.class);
+        intent.putExtra(TestService3.EXTRA_MYARG, "Hello, Service3");
         startService(intent);
     }
 
